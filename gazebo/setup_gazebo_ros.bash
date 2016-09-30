8cb5e3f089723e9e816f1f61cdfea15e892ea736
Nick Stockton <nick@uavmasterlabs@gmail.com>
2016-09-30 17:07:33 -0400

    Working sim

--- gazebo/setup_gazebo_ros.bash
+++ gazebo/setup_gazebo_ros.bash
@@ -6,7 +6,7 @@
 
 if [ "$#" -lt 1 ]
 then
-    SRC_DIR=/home/uav_master/ros_ws/src/irols
+    SRC_DIR=/home/nick/ros_ws/src/irols
 else
     SRC_DIR=$1
 fi
@@ -14,7 +14,7 @@ fi
 
 # setup Gazebo env and update package path
 export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:${SRC_DIR}/gazebo/models
-export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:${SRC_DIR}/Firmware/Tools/sitl_gazebo
+export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:${SRC_DIR}/Firmware/Tools/sitl_gazebo/build
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${SRC_DIR}/Firmware/Tools/sitl_gazebo/Build/msgs/
 export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:${SRC_DIR}
 #export GAZEBO_MODEL_DATABASE_URI=""
