5ce22feeb51375d8eb5fe6479eb5c0e77bc93d6f
Nicklas Stockton <sputnick1124@comcast.net>
2017-08-04 14:06:30 -0400

    fix imports

--- nodes/action_servers/flc_action.py
+++ nodes/action_servers/flc_action.py
@@ -2,10 +2,10 @@
 import rospy
 import actionlib
 
-import yapflm.fisyaml
-from utils import Controller
-
 import irols.msg
+
+from irols.utils import Controller
+from yapflm import fisyaml
 
 import tf2_ros
 from numpy import dot, sqrt
@@ -31,21 +31,28 @@ class DoFLCServer(object):
     def __init__(self,name):
         self._action_name = name
         self._as = actionlib.SimpleActionServer(self._action_name,
-                                                irols.msg.DoTrackAction,
+                                                irols.msg.DoFLCAction,
                                                 execute_cb=self.execute,
                                                 auto_start=False)
 
-        tf_buffer = tf2_ros.Buffer()
-        self.tf_listener = tf2_ros.TransformListener(tf_buffer)
+        self.tf_buffer = tf2_ros.Buffer()
+        tf_listener = tf2_ros.TransformListener(self.tf_buffer)
 
         self.vel_sp_pub = rospy.Publisher('mavros/setpoint_velocity/cmd_vel_unstamped',
                                       Twist,
                                       queue_size=1)
 
-        xy_dict = rospy.get_param('xy_fis')
-        z_dict = rospy.get_param('z_fis')
-        T_dict = rospy.get_param('T_fis')
-        fis_list = [fisyaml.fis_from_dict(fd) for fd in [xy_dict,xy_dict,z_dict,T_dict]]
+        fis_names = ['x_fis','y_fis','z_fis','T_fis']
+        fis_dicts = []
+        for name in fis_names:
+            if rospy.has_param(name):
+                fis_dicts.append(rospy.get_param(name))
+            else:
+                rospy.logwarn('{0}: {1} is not on param server'.format(self._action_name,name))
+        if len(fis_dicts) != len(fis_names):
+            rospy.logerr('{0}: not starting the server'.format(self._action_name))
+            return
+        fis_list = [fisyaml.fis_from_dict(fd) for fd in fis_dicts]
         self.flc = FuzzyController(fis_list,[3,3,4,2])
 
         self._as.start()
@@ -53,11 +60,11 @@ class DoFLCServer(object):
 
     def execute(self,goal):
         try:
-            err = self.tf_listener.lookup_transform('pad','base_link',rospy.Time(0))
+            err = self.tf_buffer.lookup_transform('pad','base_link',rospy.Time(0))
         except (tf2_ros.LookupException,
                 tf2_ros.ConnectivityException,
                 tf2_ros.ExtrapolationException) as e:
-            rospy.logerr('{0}: {1}'.format(self.__action_name,e))
+            rospy.logerr('{0}: {1}'.format(self._action_name,e))
             self._as.set_aborted()
             return
         
@@ -65,18 +72,26 @@ class DoFLCServer(object):
         cmd_vel = Twist()
         dist =sqrt(dot([err.x,err.y,err.z],[err.x,err.y,err.z])) 
         while dist > 0.3:
-            err = self.tf_listener.lookup_transform('pad','base_link',rospy.Time(0))
-            vx,vy,vz,dT = self.flc(err.x,err.y,err.z,err.T)
-            cmd_vel.linear.x = vx
-            cmd_vel.linear.y = vy
-            cmd_vel.linear.z = vz
-            cmd_vel.angular.z = dT
-            self.vel_sp_pub.publish(cmd_vel)
+            try:
+                err = self.tf_listener.lookup_transform('pad','base_link',rospy.Time(0))
+                vx,vy,vz,dT = self.flc(err.x,err.y,err.z,err.T)
+                cmd_vel.linear.x = vx
+                cmd_vel.linear.y = vy
+                cmd_vel.linear.z = vz
+                cmd_vel.angular.z = dT
+                self.vel_sp_pub.publish(cmd_vel)
             
-            dist =sqrt(dot([err.x,err.y,err.z],[err.x,err.y,err.z]))
-            self._feedback.dist = dist
-            self._as.publish_feedback(self._feedback)
-            rate.sleep()
+                dist =sqrt(dot([err.x,err.y,err.z],[err.x,err.y,err.z]))
+                self._feedback.dist = dist
+                self._as.publish_feedback(self._feedback)
+                rate.sleep()
+            except KeyboardInterrupt:
+                if self._as.is_active():
+                    self._as.set_aborted()
+                return
+            except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+                rate.sleep()
+                continue
         
         self._result.final_err.x = err.x
         self._result.final_err.y = err.y
--- scripts/prototyping/test_yapflm_import.py
+++ scripts/prototyping/test_yapflm_import.py
@@ -6,8 +6,9 @@ rospy.init_node('test_yapflm_import')
 
 rospy.loginfo('PYTHON_PATH:\n{}'.format(sys.path))
 
-from irols import yapflm
-import fisyaml
+import yapflm
+from yapflm import fisyaml
 rospy.loginfo("Successfully import yapflm from {}".format(yapflm.__file__))
 
+import irols.utils
 rospy.loginfo("dir(yapflm)={}".format(dir(yapflm)))
